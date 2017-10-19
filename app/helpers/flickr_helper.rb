b7767ecc4eca7626b306ce2cfc185ed2fbc125e0
Vlad <vlad@hotmail.co.nz>
2017-10-18 17:01:58 -0700

    finished

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,14 +1,14 @@
 module FlickrHelper
 	def user_photos(user_id, photo_count = 12)
-		flickr.photos.search(:user_id => user_id).values_at(0..(photo_count - 1))
+		flickr.photos.search(:user_id => user_id).first(photo_count)
 	end 
 
-	def sidebar_widget(user_id, photo_count = 12, columns = 2)
-		begin 
-			photos = user_photos(user_id, photo_count).in_groups_of(2)
-			render :partial => 'flickr/sidebar_widget', :locals => { :photos => photos }
-		rescue Exception
-			render :partial => 'flickr/unavailable'
-		end 
+	def sidebar_widget(user_id, photo_count = 12, columns = 6)
+    begin
+      photos = user_photos(user_id, photo_count).in_groups_of(6)
+      render :partial => '/flickr/sidebar_widget', :locals => { :photos => photos }
+    rescue Exception
+      render :partial => '/flickr/unavailable'
+    end
 	end
 end 	 			
